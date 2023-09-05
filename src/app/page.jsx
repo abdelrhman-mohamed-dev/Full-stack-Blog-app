43550cdf4d5671ba96941f0b558927a725300cee
boody-cpu <81966035+boody-cpu@users.noreply.github.com>
2023-09-05 13:01:43 +0300

    base css and layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,85 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href={"/"}>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+          mollitia laborum enim delectus deserunt sequi sed pariatur, ipsum eos
+          fugiat. In, a? Incidunt nostrum voluptate excepturi sunt eaque
+          consectetur consequuntur.
+        </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni
+        esse, vitae porro quibusdam consectetur deserunt eum repudiandae aperiam
+        quae quas quo suscipit alias, vero vel voluptatum expedita quisquam.
+        Aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Natus, magni esse, vitae porro quibusdam consectetur deserunt eum
+        repudiandae aperiam quae quas quo suscipit alias, vero vel voluptatum
+        expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Natus, magni esse, vitae porro quibusdam consectetur
+        deserunt eum repudiandae aperiam quae quas quo suscipit alias, vero vel
+        voluptatum expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Natus, magni esse, vitae porro quibusdam
+        consectetur deserunt eum repudiandae aperiam quae quas quo suscipit
+        alias, vero vel voluptatum expedita quisquam. Aspernatur. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Natus, magni esse, vitae
+        porro quibusdam consectetur deserunt eum repudiandae aperiam quae quas
+        quo suscipit alias, vero vel voluptatum expedita quisquam. Aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni
+        esse, vitae porro quibusdam consectetur deserunt eum repudiandae aperiam
+        quae quas quo suscipit alias, vero vel voluptatum expedita quisquam.
+        Aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Natus, magni esse, vitae porro quibusdam consectetur deserunt eum
+        repudiandae aperiam quae quas quo suscipit alias, vero vel voluptatum
+        expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Natus, magni esse, vitae porro quibusdam consectetur
+        deserunt eum repudiandae aperiam quae quas quo suscipit alias, vero vel
+        voluptatum expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Natus, magni esse, vitae porro quibusdam
+        consectetur deserunt eum repudiandae aperiam quae quas quo suscipit
+        alias, vero vel voluptatum expedita quisquam. Aspernatur. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Natus, magni esse, vitae
+        porro quibusdam consectetur deserunt eum repudiandae aperiam quae quas
+        quo suscipit alias, vero vel voluptatum expedita quisquam. Aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni
+        esse, vitae porro quibusdam consectetur deserunt eum repudiandae aperiam
+        quae quas quo suscipit alias, vero vel voluptatum expedita quisquam.
+        Aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Natus, magni esse, vitae porro quibusdam consectetur deserunt eum
+        repudiandae aperiam quae quas quo suscipit alias, vero vel voluptatum
+        expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Natus, magni esse, vitae porro quibusdam consectetur
+        deserunt eum repudiandae aperiam quae quas quo suscipit alias, vero vel
+        voluptatum expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Natus, magni esse, vitae porro quibusdam
+        consectetur deserunt eum repudiandae aperiam quae quas quo suscipit
+        alias, vero vel voluptatum expedita quisquam. Aspernatur. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Natus, magni esse, vitae
+        porro quibusdam consectetur deserunt eum repudiandae aperiam quae quas
+        quo suscipit alias, vero vel voluptatum expedita quisquam. Aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni
+        esse, vitae porro quibusdam consectetur deserunt eum repudiandae aperiam
+        quae quas quo suscipit alias, vero vel voluptatum expedita quisquam.
+        Aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Natus, magni esse, vitae porro quibusdam consectetur deserunt eum
+        repudiandae aperiam quae quas quo suscipit alias, vero vel voluptatum
+        expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Natus, magni esse, vitae porro quibusdam consectetur
+        deserunt eum repudiandae aperiam quae quas quo suscipit alias, vero vel
+        voluptatum expedita quisquam. Aspernatur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Natus, magni esse, vitae porro quibusdam
+        consectetur deserunt eum repudiandae aperiam quae quas quo suscipit
+        alias, vero vel voluptatum expedita quisquam. Aspernatur. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Natus, magni esse, vitae
+        porro quibusdam consectetur deserunt eum repudiandae aperiam quae quas
+        quo suscipit alias, vero vel voluptatum expedita quisquam. Aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magni
+        esse, vitae porro quibusdam consectetur deserunt eum repudiandae aperiam
+        quae quas quo suscipit alias, vero vel voluptatum expedita quisquam.
+        Aspernatur.
+      </Link>
+    </div>
+  );
 }
